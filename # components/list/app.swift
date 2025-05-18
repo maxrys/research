5ff03d118b46af8009bea7da913314e8f666ee01
Maxim Rysevets <root@effcore.com>
2025-05-18 15:30:50 +0300

    fix

--- # components/list/app.swift
+++ # components/list/app.swift
@@ -7,14 +7,16 @@ import SwiftUI
 
 @main struct app: App {
 
-    enum AppearanceStyle {
-        case dark, light, auto
+    enum listVariants {
+        case listItem1
+        case listItem2
+        case listItem3
     }
 
-    @State var username: String = "Max"
-    @State var isPrivate: Bool = true
-    @State private var fontSize: CGFloat = 5
-    @State private var appearance: AppearanceStyle = .auto
+    @State private var textFieldValue: String = ""
+    @State private var isChecked: Bool = true
+    @State private var sliderValue: CGFloat = 5
+    @State private var listSelection: listVariants = .listItem1
     @State private var goods = [
         "item 1",
         "item 2",
@@ -27,37 +29,42 @@ import SwiftUI
 
                 List {
                     Section {
-                        TextField("Username", text: $username)
-                        Toggle("Private Account", isOn: $isPrivate)
+                        TextField("Text field", text: self.$textFieldValue)
+                        Toggle("Checkbox", isOn: self.$isChecked)
+                        Slider(value: self.$sliderValue, in: 1 ... 10) {
+                            Label("Slider", systemImage: "text.magnifyingglass")
+                        }
+                        Picker("List", selection: self.$listSelection) {
+                            Text("listItem1").tag(listVariants.listItem1)
+                            Text("listItem2").tag(listVariants.listItem2)
+                            Text("listItem3").tag(listVariants.listItem3)
+                        }
                         Button { } label: {
-                            Text("Sign out")
+                            Text("Do something")
                         }
-                    } header: { Text("Profile") }
-
-                    Section {
-                        Slider(value: $fontSize, in: 1 ... 10) {
-                            Label("Default Font Size", systemImage: "text.magnifyingglass")
-                        }
-                        Picker("Appearance", selection: $appearance) {
-                            Text("Dark" ).tag(AppearanceStyle.dark)
-                            Text("Light").tag(AppearanceStyle.light)
-                            Text("Auto" ).tag(AppearanceStyle.auto)
-                        }
-                    } header: { Text("Appearance") }
-
-                    Section {
-                        HStack {
-                            Text("Key")
-                            Spacer()
-                            Text("Value")
-                        }
-                    } header: { Text("About") }
+                    } header: { Text("Form Elements") }
 
                     Section {
                         ForEach(self.goods.sorted(), id: \.self) { value in
                             Text(value)
                         }
-                    } header: { Text("ForEach") }
+                    } header: {
+                        Text("ForEach")
+                    } footer: {
+                        Text("Some description.")
+                    }
+
+                    Section {
+                        DisclosureGroup("Group") {
+                            ForEach(self.goods.sorted(), id: \.self) { value in
+                                Text(value)
+                            }
+                        }
+                    } header: {
+                        Text("ForEach")
+                    } footer: {
+                        Text("Some description.")
+                    }
 
                 }
 
