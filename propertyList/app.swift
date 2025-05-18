82644c5febe2b22470f3f4c2fc99c54075de8f22
Maxim Rysevets <root@effcore.com>
2025-05-18 13:59:28 +0300

    fix

---
+++ propertyList/app.swift
@@ -0,0 +1,26 @@
+
+/* ################################################################## */
+/* ### Copyright © 2024—2025 Maxim Rysevets. All rights reserved. ### */
+/* ################################################################## */
+
+import SwiftUI
+
+@main struct app: App {
+
+    var body: some Scene {
+        WindowGroup {
+            VStack {
+                Text("Property list")
+            }
+        }
+    }
+
+    init() {
+
+        dump(
+            ModelDemo.select()
+        )
+
+    }
+
+}
