19438fa0813df48f081d4dce546b4b690175d856
Maxim Rysevets <root@effcore.com>
2025-06-24 11:25:52 +0300

    fix

--- # components/Windows/app.swift
+++ # components/Windows/app.swift
@@ -10,18 +10,18 @@ import SwiftUI
     @Environment(\.openWindow) private var openWindow
 
     var body: some Scene {
-        WindowGroup(for: String.self) { $windowId in
-            if let windowId {
+        WindowGroup(for: String.self) { $value in
+            if let value {
                 /* MARK: Child Windows */
                 ContentView(
-                    windowId: windowId
+                    windowId: value
                 )
             } else {
                 /* MARK: Parent Window */
                 VStack {
-                    Button("Open in New Window 1") { openWindow(value: "1") }
-                    Button("Open in New Window 2") { openWindow(value: "2") }
-                    Button("Open in New Window 3") { openWindow(value: "3") }
+                    Button("Open Window 1") { openWindow(value: "1") }
+                    Button("Open Window 2") { openWindow(value: "2") }
+                    Button("Open Window 3") { openWindow(value: "3") }
                 }.padding(20)
             }
         }.windowResizability(.contentSize)
@@ -30,13 +30,13 @@ import SwiftUI
 }
 
 struct ContentView: View {
-    @State private var value: UInt = 0
+    @State private var counter: UInt = 0
     let windowId: String
     var body: some View {
         VStack {
             Text("Window ID: \(self.windowId)")
-            Button("Increment") { self.value += 1 }
-            Text("Counter: \(self.value)")
+            Button("Increment") { self.counter += 1 }
+            Text("Counter: \(self.counter)")
         }.padding(20)
     }
 }
