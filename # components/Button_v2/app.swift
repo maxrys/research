7702fe81c8495117fd63677b42acdd58b82fe046
Maxim Rysevets <root@effcore.com>
2025-06-27 12:27:49 +0300

    fix

---
+++ # components/Button_v2/app.swift
@@ -0,0 +1,26 @@
+
+/* ################################################################## */
+/* ### Copyright © 2024—2025 Maxim Rysevets. All rights reserved. ### */
+/* ################################################################## */
+
+import SwiftUI
+
+@main struct app: App {
+
+    var body: some Scene {
+        WindowGroup {
+            VStack {
+                ButtonCustom()
+                ButtonCustom(flexibility: .none)
+                ButtonCustom(flexibility: .size(100))
+                ButtonCustom(flexibility: .infinity)
+                ButtonCustom(style: .accent)
+                ButtonCustom(style: .custom)
+            }.padding(10)
+        }
+    }
+
+    init() {
+    }
+
+}
