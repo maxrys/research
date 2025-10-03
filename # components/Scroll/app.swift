7274b17f0e258ff9e6315150fcc0dbf5749fd7b6
Maxim Rysevets <root@effcore.com>
2025-10-03 10:59:31 +0300

    fix

--- # components/Scroll/app.swift
+++ # components/Scroll/app.swift
@@ -9,21 +9,7 @@ import SwiftUI
 
     var body: some Scene {
         WindowGroup {
-            ScrollView([.horizontal]) {
-                LazyHStack(spacing: 10) {
-                    ForEach(0 ..< 100, id: \.self) { index in
-                        RoundedRectangle(cornerRadius: 10)
-                            .fill(.blue)
-                            .frame(width: 200, height: 150)
-                            .overlay(
-                                Text("Item \(index)").foregroundColor(.white)
-                            )
-                    }
-                }.scrollTargetLayout()
-            }
-            .scrollTargetBehavior(.viewAligned)
-            .padding(10)
-            .frame(width: 400)
+            SnapToElement()
         }.windowResizability(.contentSize)
     }
 
