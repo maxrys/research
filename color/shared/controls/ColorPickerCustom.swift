b861413d666004f9083ed95beea7e5db08c869df
Maxim Rysevets <root@effcore.com>
2025-11-15 12:00:50 +0300

    fix

--- color/shared/controls/ColorPickerCustom.swift
+++ color/shared/controls/ColorPickerCustom.swift
@@ -11,9 +11,6 @@ struct ColorPickerCustom: View {
     static let ROWS = 50
     static let CELL_SIZE = 5
 
-    @State var saturation: Decimal = 0.5
-    @State var brightness: Decimal = 0.5
-
     init() {
     }
 
@@ -22,53 +19,44 @@ struct ColorPickerCustom: View {
         Canvas { context, size in
             for y in 0 ... Self.ROWS {
             for x in 0 ... Self.COLS {
-                let hue        = Double(x) / Double(Self.COLS)
-                let brightness = Double(y) / Double(Self.ROWS)
+                let H: Decimal = Decimal(y) / Decimal(Self.COLS)
+                let S: Decimal = 1.0
+                let B: Decimal = Decimal(x) / Decimal(Self.ROWS)
                 context.drawRectangle(
                     x: Double(Self.CELL_SIZE * x),
                     y: Double(Self.CELL_SIZE * y),
                     w: Double(Self.CELL_SIZE),
                     h: Double(Self.CELL_SIZE),
                     colorFill: Color(
-                        hue       : hue,
-                        saturation: self.saturation.double,
-                        brightness: self.brightness.double
+                        hue       : H.double,
+                        saturation: S.double,
+                        brightness: B.double
                     )
                 )
-                print(
-                    "H: \(self.formatDouble(hue)) | " +
-                    "S: \(self.formatDouble(brightness)) | " +
-                    "B: \(self.formatDouble(self.saturation.double))"
+            }}
+            for y in 0 ... Self.ROWS {
+            for x in 0 ... Self.COLS {
+                let H: Decimal = Decimal(y) / Decimal(Self.COLS)
+                let S: Decimal = Decimal(x) / Decimal(Self.ROWS)
+                let B: Decimal = 1.0
+                context.drawRectangle(
+                    x: Double(Self.CELL_SIZE * x) + Double(Self.CELL_SIZE * (Self.COLS + 1)),
+                    y: Double(Self.CELL_SIZE * y),
+                    w: Double(Self.CELL_SIZE),
+                    h: Double(Self.CELL_SIZE),
+                    colorFill: Color(
+                        hue       :     H.double,
+                        saturation: 1 - S.double,
+                        brightness:     B.double
+                    )
                 )
             }}
         }
         .frame(
-            width : Double(Self.CELL_SIZE * (Self.COLS + 1)),
+            width : Double(Self.CELL_SIZE * (Self.COLS + 1)) * 2,
             height: Double(Self.CELL_SIZE * (Self.ROWS + 1))
         )
 
-        HStack(spacing: 10) {
-            Text("saturation").font(.headline)
-                Button { if self.saturation > 0 {self.saturation -= 0.1} } label: { Text("-") }
-                Button { if self.saturation < 1 {self.saturation += 0.1} } label: { Text("+") }
-            Text(self.formatDouble(self.saturation.double))
-        }
-
-        HStack {
-            Text("brightness").font(.headline)
-                Button { if self.brightness > 0 {self.brightness -= 0.1} } label: { Text("-") }
-                Button { if self.brightness < 1 {self.brightness += 0.1} } label: { Text("+") }
-            Text(self.formatDouble(self.brightness.double))
-        }
-
-    }
-
-    func formatDouble(_ value: Double, fractionLength: Int = 3) -> String {
-        value.formatted(
-            .number.precision(
-                .fractionLength(fractionLength)
-            )
-        )
     }
 
 }
