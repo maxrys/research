ba5ff0448c3f62dd6e82b3115ba26cd4215d6942
Maxim Rysevets <root@effcore.com>
2025-11-17 22:58:48 +0300

    fix

--- color/shared/controls/ColorPickerCustom.swift
+++ color/shared/controls/ColorPickerCustom.swift
@@ -7,12 +7,18 @@ import SwiftUI
 
 struct ColorPickerCustom: View {
 
-    static let COLS = 20
-    static let ROWS = 20
-    static let CELL_SIZE = 15
+    static let COLS = 10
+    static let ROWS = 10
+    static let CELL_SIZE = 30
 
     init() {
     }
+
+    func getColor(_ colNum: Int, _ rowNum: Int) -> (H: Decimal, S: Decimal, B: Decimal) {(
+        H:                            Decimal(rowNum            ) / Decimal(Self.ROWS),
+        S: colNum > Self.COLS ? 1.0 - Decimal(colNum - Self.COLS) / Decimal(Self.COLS) : 1.0,
+        B: colNum > Self.COLS ? 1.0 : Decimal(colNum            ) / Decimal(Self.COLS)
+    )}
 
     public var body: some View {
 
@@ -22,32 +28,30 @@ struct ColorPickerCustom: View {
         Canvas { context, size in
             for rowNum in 0 ... Self.ROWS     {
             for colNum in 0 ... Self.COLS * 2 {
-                let isHalf = colNum > Self.COLS
-                let H =                Decimal(rowNum            ) / Decimal(Self.ROWS)
-                let S = isHalf ? 1.0 - Decimal(colNum - Self.COLS) / Decimal(Self.COLS) : 1.0
-                let B = isHalf ? 1.0 : Decimal(colNum            ) / Decimal(Self.COLS)
+                let color = self.getColor(colNum, rowNum)
                 context.drawRectangle(
                     x: Double(Self.CELL_SIZE * colNum),
                     y: Double(Self.CELL_SIZE * rowNum),
                     w: Double(Self.CELL_SIZE),
                     h: Double(Self.CELL_SIZE),
                     colorFill: Color(
-                        hue       : H.double,
-                        saturation: S.double,
-                        brightness: B.double
+                        hue       : color.H.double,
+                        saturation: color.S.double,
+                        brightness: color.B.double
                     )
                 )
             }}
         }
         .frame(
-            width : canvasW * 2,
+            width : canvasW * 2 - Double(Self.CELL_SIZE),
             height: canvasH
         )
         .pointerStyle(.link)
         .onTapGesture { location in
-            let colNum: UInt = UInt(location.x / CGFloat(Self.CELL_SIZE))
-            let rowNum: UInt = UInt(location.y / CGFloat(Self.CELL_SIZE))
-            print("colNum: \(colNum) | rowNum: \(rowNum)")
+            let colNum = Int(location.x / CGFloat(Self.CELL_SIZE))
+            let rowNum = Int(location.y / CGFloat(Self.CELL_SIZE))
+            let color = self.getColor(colNum, rowNum)
+            print("colNum: \(colNum) | rowNum: \(rowNum) | H: \(color.H) | S: \(color.S) | B: \(color.B)")
         }
 
     }
