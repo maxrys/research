58d89610f90333b2bafbead3f819b12d861de60d
Maxim Rysevets <root@effcore.com>
2025-12-07 00:08:51 +0300

    fix

---
+++ # gui/ConfirmationDialog/app.swift
@@ -0,0 +1,56 @@
+
+/* ################################################################## */
+/* ### Copyright © 2024—2025 Maxim Rysevets. All rights reserved. ### */
+/* ################################################################## */
+
+import SwiftUI
+
+@main struct ThisApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            Main()
+        }
+    }
+
+}
+
+struct Main: View {
+
+    @State var isShowingPopover = false
+
+    var body: some View {
+        Button("Show Popover") {
+            self.isShowingPopover = true
+        }
+        .popover(isPresented: self.$isShowingPopover) {
+            Popover(isShowingPopover: self.$isShowingPopover)
+        }
+    }
+
+}
+
+struct Popover: View {
+
+    @Binding var isShowingPopover: Bool
+    @State var isShowingDialog = false
+
+    var body: some View {
+        Button("Show dialog") {
+            self.isShowingDialog = true
+        }
+        .padding(20)
+        .confirmationDialog("What to do?", isPresented: self.$isShowingDialog) {
+            Button("Close Dialog", role: .cancel) {
+                self.isShowingDialog = false
+            }
+            Button("Close Dialog + Popover", role: .destructive) {
+                self.isShowingDialog = false
+                DispatchQueue.main.async {
+                    self.isShowingPopover = false
+                }
+            }
+        }
+    }
+
+}
