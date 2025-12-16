e09c652fbe007c7b0ffe7efe4e01c9a1aecd92f1
Maxim Rysevets <root@effcore.com>
2025-12-17 00:43:20 +0300

    fix

--- yield/app.swift
+++ yield/app.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
     var body: some Scene {
         Window("Main", id: "main") {
+
+            TimelineView(.periodic(from: .now, by: 1.0 / 24)) { context in
+                Image(systemName: "arrow.trianglehead.2.clockwise.rotate.90.circle")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .rotationEffect(.degrees(
+                        Double(UInt(context.date.timeIntervalSince1970 * 500) % 360)
+                    ))
+            }
+
             ProgressDemo()
+
         }
     }
 
