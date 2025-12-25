580809d571f7cd7316779b5b39bae79dab87891c
Maxim Rysevets <root@effcore.com>
2025-12-26 00:37:11 +0300

    fix

--- # gui/Tabs/controls/TabsCustom.swift
+++ # gui/Tabs/controls/TabsCustom.swift
@@ -103,28 +103,6 @@ struct TabsCustom: View {
 
 }
 
-struct TabItemCustom: View {
-
-    var title: String
-    var systemIcon: String?
-    var view: any View
-
-    init(
-        title: String,
-        systemIcon: String?,
-        @ViewBuilder view: () -> any View
-    ) {
-        self.title = title
-        self.systemIcon = systemIcon
-        self.view = view()
-    }
-
-    public var body: some View {
-        AnyView(self.view)
-    }
-
-}
-
 
 
 /* ############################################################# */
