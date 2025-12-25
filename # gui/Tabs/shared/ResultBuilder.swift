d3c47c83d66e1c7b850132ad601b0b55f7b6a465
Maxim Rysevets <root@effcore.com>
2025-12-26 00:33:12 +0300

    fix

--- # gui/Tabs/shared/ResultBuilder.swift
+++ # gui/Tabs/shared/ResultBuilder.swift
@@ -3,8 +3,6 @@
 /* ### Copyright © 2026 Maxim Rysevets. All rights reserved. ### */
 /* ############################################################# */
 
-import SwiftUI
-
 @resultBuilder struct ViewBuilderArray<T> {
 
     static func buildBlock(_ components: T...) -> [T] {
--- # gui/resultBuilder/extensions/Array.swift
+++ # gui/resultBuilder/extensions/Array.swift
@@ -3,8 +3,6 @@
 /* ### Copyright © 2026 Maxim Rysevets. All rights reserved. ### */
 /* ############################################################# */
 
-import SwiftUI
-
 extension Array {
 
     subscript(safe index: Int) -> Element? {
