91d17fef9e72ef4d6c3fb2654285a0fe1ddd3264
Maxim Rysevets <root@effcore.com>
2026-02-15 23:40:08 +0300

    fix

---
+++ # gui/MessageBox/controls/MessageBox/MessageLifecycle.swift
@@ -0,0 +1,15 @@
+
+
+/* ############################################################# */
+/* ### Copyright © 2026 Maxim Rysevets. All rights reserved. ### */
+/* ############################################################# */
+
+import SwiftUI
+
+enum MessageLifecycle {
+
+    case persistent
+    case inProgress(Double)
+    case expired
+
+}
