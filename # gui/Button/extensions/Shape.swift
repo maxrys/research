ccb426a2fe86714963e9aec88632e4c5e1bd1589
Maxim Rysevets <root@effcore.com>
2026-02-19 15:48:02 +0300

    fix

--- # gui/Button/extensions/Shape.swift
+++ # gui/Button/extensions/Shape.swift
@@ -8,8 +8,19 @@ import SwiftUI
 extension Shape {
 
     @ViewBuilder func fillGradientPolyfill(_ color: Color) -> some View {
-        if #available(macOS 13.0, *) { self.fill(color.gradient) }
-        else                         { self.fill(color) }
+        if #available(macOS 13.0, *) {
+            self.fill(
+                color.gradient
+            )
+        } else {
+            self.fill(
+                    LinearGradient(
+                        colors: [color.opacity(0.7), color],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+        }
     }
 
 }
