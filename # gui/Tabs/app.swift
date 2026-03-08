91e35dbdeb7537123fa64ad3f8262173e9df6e8d
Maxim Rysevets <root@effcore.com>
2026-03-08 19:08:11 +0300

    fix

--- # gui/Tabs/app.swift
+++ # gui/Tabs/app.swift
@@ -10,9 +10,9 @@ import SwiftUI
     var body: some Scene {
         WindowGroup {
             TabCustom {
-                TabCustom_item(title: NSLocalizedString("Update", comment: ""), icon: Image(systemName: "pencil.tip.crop.circle")) { Text("Tab Update content").padding(20) }
-                TabCustom_item(title: NSLocalizedString("Insert", comment: ""), icon: Image(systemName: "plus.circle"           )) { Text("Tab Insert content").padding(20) }; TabCustom_spacer()
-                TabCustom_item(title: NSLocalizedString("Delete", comment: ""), icon: Image(systemName: "trash"                 )) { Text("Tab Delete content").padding(20) }
+                TabCustom_Item(title: NSLocalizedString("Update", comment: ""), icon: Image(systemName: "pencil.tip.crop.circle")) { Text("Tab Update content").padding(20) }
+                TabCustom_Item(title: NSLocalizedString("Insert", comment: ""), icon: Image(systemName: "plus.circle"           )) { Text("Tab Insert content").padding(20) }; TabCustom_Spacer()
+                TabCustom_Item(title: NSLocalizedString("Delete", comment: ""), icon: Image(systemName: "trash"                 )) { Text("Tab Delete content").padding(20) }
             }
         }
     }
