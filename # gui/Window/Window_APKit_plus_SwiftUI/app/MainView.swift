5f842ab01eb92cca23e899d768423baf42f8b1e8
Maxim Rysevets <root@effcore.com>
2026-03-16 01:39:00 +0300

    fix

--- # gui/Window/Window_APKit_plus_SwiftUI/app/MainView.swift
+++ # gui/Window/Window_APKit_plus_SwiftUI/app/MainView.swift
@@ -9,11 +9,14 @@ struct MainView: View {
 
     var body: some View {
         VStack {
-            Button("Open Window ID = 1") { NSWindow.show(ID: "id=1") }
-            Button("Open Window ID = 2") { NSWindow.show(ID: "id=2") }
-            Button("Open Window ID = 3") { NSWindow.show(ID: "id=3") }
+            Button("Open Window ID = 1") { NSWindow.show("id=1") }
+            Button("Open Window ID = 2") { NSWindow.show("id=2") }
+            Button("Open Window ID = 3") { NSWindow.show("id=3") }
+            Button("Hide Window with animation") { NSWindow.hideWithAnimation(App.MAIN_WINDOW_ID) }
+            Button("Hide Title Icons") { NSWindow.get(App.MAIN_WINDOW_ID)?.hideTitleButtons(isVisible: false) }
         }
         .padding(20)
+        .frame(width: 300)
     }
 
 }
--- # gui/Window/Window_APKit_plus_SwiftUI/app/app.swift
+++ # gui/Window/Window_APKit_plus_SwiftUI/app/app.swift
@@ -7,7 +7,10 @@ import Cocoa
 
 @main final class App: NSObject, NSApplicationDelegate, NSWindowDelegate {
 
-    @MainActor private static var appDelegate: App!
+    static public let MAIN_WINDOW_ID = "main"
+    static public let MAIN_WINDOW_TITLE = "Custom Window"
+
+    @MainActor public static var appDelegate: App!
 
     static func main() {
         let app = NSApplication.shared
@@ -17,23 +20,17 @@ import Cocoa
     }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        NSWindow.makeAndShowFromSwiftUIView(ID: "main", title: "Main Window" , isVisible: true , delegate: self, view: MainView())
-        NSWindow.makeAndShowFromSwiftUIView(ID: "id=1", title: "Child Window", isVisible: false, delegate: self, view: ChildView(windowId: "id=1"))
-        NSWindow.makeAndShowFromSwiftUIView(ID: "id=2", title: "Child Window", isVisible: false, delegate: self, view: ChildView(windowId: "id=2"))
-        NSWindow.makeAndShowFromSwiftUIView(ID: "id=3", title: "Child Window", isVisible: false, delegate: self, view: ChildView(windowId: "id=3"))
+        _ = NSWindow.makeAndShowFromSwiftUIView(ID: "main", title: Self.MAIN_WINDOW_TITLE, isVisible: true, delegate: self, view: MainView())
+        _ = NSWindow.makeAndShowFromSwiftUIView(ID: "id=1", title: "Child Window", isVisible: false, delegate: self, view: ChildView(windowId: "id=1"))
+        _ = NSWindow.makeAndShowFromSwiftUIView(ID: "id=2", title: "Child Window", isVisible: false, delegate: self, view: ChildView(windowId: "id=2"))
+        _ = NSWindow.makeAndShowFromSwiftUIView(ID: "id=3", title: "Child Window", isVisible: false, delegate: self, view: ChildView(windowId: "id=3"))
     }
 
-    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-        return true
-    }
-
-    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        return false
-    }
-
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool { return true }
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool { return false }
     func applicationShouldHandleReopen(_ app: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
         // if (!flag) {
-            NSWindow.show(ID: "main")
+            NSWindow.show(Self.MAIN_WINDOW_ID)
         // }
         return true
     }
