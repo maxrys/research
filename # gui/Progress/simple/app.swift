f84028b162cf5195fba598b71f012f80cae88e1f
Maxim Rysevets <root@effcore.com>
2026-05-01 11:49:11 +0300

    fix

---
+++ # gui/Progress/simple/app.swift
@@ -0,0 +1,21 @@
+
+/* ################################################################## */
+/* ### Copyright © 2024—2025 Maxim Rysevets. All rights reserved. ### */
+/* ################################################################## */
+
+import SwiftUI
+
+@main struct ThisApp: App {
+
+    public var body: some Scene {
+        WindowGroup {
+            VStack(spacing: 10) {
+                ForEach(Array(stride(from: -0.1, through: 1.1, by: 0.1)), id: \.self) { value in
+                    ProgressSimple(value: value)
+                }
+            }
+            .padding(10)
+        }
+    }
+
+}
