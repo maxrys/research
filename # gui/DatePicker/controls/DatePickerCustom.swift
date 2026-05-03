ddd4ba4ddfaf285eee37e87555267cdb4da8975f
Maxim Rysevets <root@effcore.com>
2026-05-03 21:25:41 +0300

    fix

--- # gui/DatePicker/controls/DatePickerCustom.swift
+++ # gui/DatePicker/controls/DatePickerCustom.swift
@@ -17,7 +17,7 @@ struct DatePickerCustom: View {
     @State private var hour: Int   /* 0 ... 23 */
     @State private var minute: Int /* 0 ... 59 */
     @State private var second: Int /* 0 ... 59 */
-    @State private var zone: Int
+    @State private var zone: String
 
     private let yearMinValue: Int
     private let yearMaxValue: Int
@@ -34,7 +34,7 @@ struct DatePickerCustom: View {
         self.hour   = value.wrappedValue.hour
         self.minute = value.wrappedValue.minute
         self.second = value.wrappedValue.second
-        self.zone   = 0
+        self.zone   = "UTC"
         self.yearMinValue = yearMinValue
         self.yearMaxValue = yearMaxValue
     }
@@ -90,11 +90,8 @@ struct DatePickerCustom: View {
             Text(NSLocalizedString("TimeZone", comment: ""))
                 .font(.headline)
 
-            DatePickerCustom.FieldList(
-                toValue: self.$zone,
-                items: Date.TIME_ZONES.reduce(into: [Int: String](), { result, item in
-                    result[result.count] = item.name
-                })
+            DatePickerCustom.FieldTimeZone(
+                toValue: self.$zone
             ).frame(width: 180)
 
             Text("")
@@ -111,7 +108,9 @@ struct DatePickerCustom: View {
         .onChange(of: self.hour  ) { newHourValue   in self.value.hour   = newHourValue }
         .onChange(of: self.minute) { newMinuteValue in self.value.minute = newMinuteValue }
         .onChange(of: self.second) { newSecondValue in self.value.second = newSecondValue }
-        .onChange(of: self.zone  ) { newZoneValue   in }
+        .onChange(of: self.zone  ) { newZoneValue   in
+            dump("\(newZoneValue) \(Date.TIME_ZONES_OFFSSET[newZoneValue])")
+        }
     }
 
     private struct FieldRange: View {
@@ -133,6 +132,23 @@ struct DatePickerCustom: View {
             Picker("", selection: self.$toValue) {
                 ForEach(Array(self.items.sorted(by: { (lhs, rhs) in lhs.key < rhs.key }).enumerated()), id: \.element.key) { index, element in
                     Text("\(String(element.value))")
+                }
+            }
+        }
+    }
+
+    private struct FieldTimeZone: View {
+        @Binding public var toValue: String
+        var body: some View {
+            Picker("", selection: self.$toValue) {
+                let groups = Date.TIME_ZONES_GROUPPED_LIST.sorted(by: { (lhs, rhs) in lhs.key > rhs.key })
+                ForEach(groups, id: \.key) { offsetNumeric, group in
+                    Section(header: Text(group.offsetFormatted)) {
+                        let zones = group.items.sorted(by: { (lhs, rhs) in lhs.key < rhs.key })
+                        ForEach(zones, id: \.key) { ID, title in
+                            Text(title).id(ID)
+                        }
+                    }
                 }
             }
         }
