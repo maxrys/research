d96915f446d36c6e761eccecdddb543edad6423e
Maxim Rysevets <root@effcore.com>
2026-06-29 14:40:17 +0300

    fix

--- # gui/Table/components/TableCustom/TableCustom.swift
+++ # gui/Table/components/TableCustom/TableCustom.swift
@@ -189,7 +189,7 @@ struct TableCustom_Previews1: PreviewProvider {
     struct ViewWithState: View {
         @State private var selected: Set<Int> = [4]
         var body: some View {
-            Previewer (isHorizontal: true, padding: 20) {
+            Previewer (axis: .horizontal, padding: 20) {
                 TableCustom(
                     selected: self.$selected,
                     isVisibleHeader: true,
@@ -225,7 +225,7 @@ struct TableCustom_Previews2: PreviewProvider {
     struct ViewWithState: View {
         @State private var selected: Set<Int> = [4]
         var body: some View {
-            Previewer (isHorizontal: true, padding: 20) {
+            Previewer (axis: .horizontal, padding: 20) {
                 TableCustom(
                     selected: self.$selected,
                     isVisibleHeader: true,
