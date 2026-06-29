088c790427c3c4d15ca0b88b53a99347407046af
Maxim Rysevets <root@effcore.com>
2026-06-29 12:41:59 +0300

    fix

--- # gui/Table/extensions/Color.swift
+++ # gui/Table/extensions/Color.swift
@@ -7,6 +7,16 @@ import SwiftUI
 
 extension Color {
 
+    struct NSColorSet {
+        subscript(_ keyPath: KeyPath<NSColor.Type, NSColor>) -> Color {
+            Color(NSColor.self[
+                keyPath: keyPath
+            ])
+        }
+    }
+
+    static let NS = NSColorSet()
+
     static let label: Color = {
         Color(NSColor.labelColor)
     }()
