d3374817aa99e1402d4387131a642583c7daa5b6
Maxim Rysevets <root@effcore.com>
2026-09-22 16:40:46 +0300

    fix

--- # gui/MessageBox/new/types/_const.swift
+++
@@ -1,8 +0,0 @@
-
-/* ############################################################# */
-/* ### Copyright © 2026 Maxim Rysevets. All rights reserved. ### */
-/* ############################################################# */
-
-import Foundation
-
-let APP_ID = "TestMessageBox"
